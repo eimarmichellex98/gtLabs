4401edfcde9b9246e375c2afa31bc6f850346261
EIMAR QUINN <G00374109@gmit.ie>
2021-02-17 19:33:58 +0000

    test

---
+++ basics.py
@@ -0,0 +1,14 @@
+# Eimar Michelle Quinn
+# Python Basics
+# 17/02/2021
+
+print("Hello, World!")
+
+a = 1
+b = 1.0
+s = "Hello, World from a string!"
+t = 'Hello, from a different string'
+
+print(a, b, s, t)
+
+print(s[:2.])
